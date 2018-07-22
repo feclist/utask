90db6e7cfa4878de54b41aee5d3ae3338a8637a2
Floris List <floris.list@gmail.com>
2018-07-22 18:17:03 -0400

    - Created fixtures - Created initial DB model - Created initial migrations
    TODO:
    - Create serializers
    - Create api routes

--- utask/api/models.py
+++ utask/api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from datetime import datetime
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_id = models.TextField(blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Task(models.Model):
+    created_time = models.DateTimeField(default=datetime.utcnow())
+    end_time = models.DateTimeField()
+    summary = models.TextField(blank=True)
+    reward = models.FloatField(blank=True)
+    title = models.TextField()
+    type = models.TextField()
+    total_cost = models.IntegerField()
+    amount = models.IntegerField()
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    completions = models.ManyToManyField(User, related_name='completions')
--- utask/utask/settings.py
+++ utask/utask/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'utask',
     'rest_framework',
     'frontend',
+    'api'
 ]
 
 MIDDLEWARE = [
