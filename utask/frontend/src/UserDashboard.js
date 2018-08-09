824574154dee0c270f599a312ab1fba4a273493c
Floris List <floris.list@gmail.com>
2018-08-09 18:47:20 +0200

    - Initial Wallet component

--- utask/frontend/src/UserDashboard.js
+++ utask/frontend/src/UserDashboard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { Tabs } from '../node_modules/@material-ui/core';
+import { Tabs, Grid } from '../node_modules/@material-ui/core';
 import Tab from '@material-ui/core/Tab';
 import TransactionList from './components/TransactionList';
 import TaskList from './components/TaskList';
+import Wallet from './components/Wallet';
 
 
 const styles = theme => ({
     root: {
         marginTop: 12,
-        width: 800,
         margin: 'auto',
     }
 })
@@ -30,18 +30,25 @@ class UserDashboard extends React.Component {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
-                <Tabs
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    fullWidth
-                >
-                    <Tab label="Tasks" value={0} />
-                    <Tab label="Transaction history" value={1} />
-                </Tabs>
-                <TaskList style={{display: this.state.value === 0 ? 'block' : 'none'}} />
-                <TransactionList style={{display: this.state.value === 1 ? 'block' : 'none'}} />
+                <Grid container spacing={16}>
+                    <Grid item xs={8}>
+                        <Tabs
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            indicatorColor="primary"
+                            textColor="primary"
+                            fullWidth
+                        >
+                            <Tab label="Tasks" value={0} />
+                            <Tab label="Transaction history" value={1} />
+                        </Tabs>
+                        <TaskList style={{ display: this.state.value === 0 ? 'block' : 'none' }} />
+                        <TransactionList style={{ display: this.state.value === 1 ? 'block' : 'none' }} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Wallet />
+                    </Grid>
+                </Grid>
             </div>
         )
     }
