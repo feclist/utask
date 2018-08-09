fe1eafbbc40044cfd3b1f49512d2fc9f78fff45d
Shane Koppers <shanekoppers@hotmail.com>
2018-08-09 20:01:03 -0200

    remove react-router-redux from reducers

--- utask/frontend/src/reducers/index.js
+++ utask/frontend/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import account from './account'
-import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
-  account,
-  router: routerReducer
+  account
 })
